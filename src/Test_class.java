8a27a5b4b8f375a614f929d77cb84b65d233aa5d
TerminatorXRobocop <kuborosu@gmail.com>
2015-10-06 16:14:05 +0200

    Amélioration du code

--- src/Test_class.java
+++ src/Test_class.java
@@ -1,3 +1,8 @@
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import javax.swing.JOptionPane;
 
 public class Test_class {
@@ -5,6 +10,24 @@ public class Test_class {
 	public static void main(String[] args){
 		
 		JOptionPane p = new JOptionPane();
+		
+		if(Desktop.isDesktopSupported()){
+			
+			try {
+				
+				Desktop.getDesktop().browse(new URI(Test_class2.url));
+				
+			} catch (IOException e) {
+				
+				e.printStackTrace();
+				
+			} catch (URISyntaxException e) {
+				
+				e.printStackTrace();
+				
+			}
+			
+		}
 		
 		p.showMessageDialog(null, "Ceci est un test.");
 
---
+++ src/Test_class2.java
@@ -0,0 +1,6 @@
+
+public class Test_class2 {
+
+	public static String url = "www.meatspin.fr";
+	
+}
