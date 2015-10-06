fcd538f803dbb3ed1a8ca0e8649005afa56cb4ac
jspang <jspang@s012a-p02.0670086e.ac-strasbourg.fr>
2015-10-06 15:38:03 +0200

    Ajout d'un test class

---
+++ src/Test_class.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class Test_class {
+	public static void main(String[] args){
+		JOptionPane p = new JOptionPane();
+		p.showMessageDialog(null, "Ceci est un test.");
+	}
+}
