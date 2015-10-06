cdd54a6322a67e0b824d684dd254bc75154e7fab
TerminatorXRobobop <kuborosu@gmail.com>
2015-10-06 15:49:58 +0200

    Modif mienure

--- src/Test_class.java
+++ src/Test_class.java
@@ -1,6 +1,7 @@
 import javax.swing.JOptionPane;
 
 public class Test_class {
+	
 	public static void main(String[] args){
 		JOptionPane p = new JOptionPane();
 		p.showMessageDialog(null, "Ceci est un test.");
