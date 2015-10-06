1a12a20c47fc49cad18d64f58c4397e758c4c2fd
TerminatorXRobobop <kuborosu@gmail.com>
2015-10-06 15:43:32 +0200

    Comit & push avec le bon profil

--- src/Test_class.java
+++ src/Test_class.java
@@ -4,5 +4,7 @@ public class Test_class {
 	public static void main(String[] args){
 		JOptionPane p = new JOptionPane();
 		p.showMessageDialog(null, "Ceci est un test.");
+		//modification du contenu de la classe
+		
 	}
 }
