62195d0ab740c0173240896ccc47c36a6833e18b
TerminatorXRobobop <kuborosu@gmail.com>
2015-10-06 15:48:39 +0200

    Modif mineure

--- src/Test_class.java
+++ src/Test_class.java
@@ -4,7 +4,5 @@ public class Test_class {
 	public static void main(String[] args){
 		JOptionPane p = new JOptionPane();
 		p.showMessageDialog(null, "Ceci est un test.");
-		//modification du contenu de la classe
-		
 	}
 }
