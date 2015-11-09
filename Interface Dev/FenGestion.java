ad808da64f268208ae152c27023c6676ee3fa2e6
TerminatorXRobocop <kuborosu@gmail.com>
2015-11-09 11:41:04 +0100

    Une modif majeur de l'interface

--- Interface Dev/FenGestion.java
+++ Interface Dev/FenGestion.java
@@ -3,8 +3,13 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 
@@ -12,7 +17,7 @@ public class FenGestion extends JFrame{
 	
 	private JButton[] buttons = new JButton[20]; 
 	private JPanel p = new JPanel();
-	private GridLayout l = new GridLayout(0,10);
+	private GridLayout l = new GridLayout(0,10,10,10);
 	
 	public FenGestion(){
 		//preprossesing
@@ -21,6 +26,44 @@ public class FenGestion extends JFrame{
 		this.getContentPane().add(p);
 		p.setLayout(l);
 		initButtons();
+		// Creates a menubar for a JFrame
+        JMenuBar menuBar = new JMenuBar();
+         
+        // Add the menubar to the frame
+        setJMenuBar(menuBar);
+         
+        // Define and add two drop down menu to the menubar
+        JMenu fileMenu = new JMenu("File");
+        JMenu editMenu = new JMenu("Edit");
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+         
+        // Create and add simple menu item to one of the drop down menu
+        JMenuItem newAction = new JMenuItem("New");
+        JMenuItem openAction = new JMenuItem("Open");
+        JMenuItem exitAction = new JMenuItem("Exit");
+        JMenuItem cutAction = new JMenuItem("Cut");
+        JMenuItem copyAction = new JMenuItem("Copy");
+        JMenuItem pasteAction = new JMenuItem("Paste");
+         
+        // Create a ButtonGroup and add both radio Button to it. Only one radio
+        // button in a ButtonGroup can be selected at a time.
+        ButtonGroup bg = new ButtonGroup();
+        fileMenu.add(newAction);
+        fileMenu.add(openAction);
+        fileMenu.addSeparator();
+        fileMenu.add(exitAction);
+        editMenu.add(cutAction);
+        editMenu.add(copyAction);
+        editMenu.add(pasteAction);
+        editMenu.addSeparator();
+        // Add a listener to the New menu item. actionPerformed() method will
+        // invoked, if user triggred this menu item
+        newAction.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                System.out.println("You have clicked on the new action");
+            }
+        });
 		//post processing
 		this.pack();
 		this.setLocationRelativeTo(null);
@@ -31,11 +74,11 @@ public class FenGestion extends JFrame{
 		for(int i = 0; i < buttons.length; i++){
 			buttons[i] = new JButton();
 			buttons[i].setText(i+"");
+			//buttons[i].setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 			buttons[i].addActionListener(new ActionListener() {
 				
 				@Override
 				public void actionPerformed(ActionEvent arg0) {
-					// TODO Auto-generated method stub
 					
 				}
 			});
