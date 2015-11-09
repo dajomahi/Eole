85494054f5a899725dfe08104df825a61ca40dfc
Maxime Moser <maxmaxmoser@gmail.com>
2015-11-09 11:14:56 +0100

    Création de l'éventuel constructeur

--- AjoutVoilier/Voilier.java
+++ AjoutVoilier/Voilier.java
@@ -1,4 +1,13 @@
 
 public class Voilier {
-
+	private int numVoilier;
+	private String nomVoilier;
+	private int classeVoilier;
+	private int rating;
+	
+	public Voilier(int numVoilier, String nomVoilier, int classeVoilier, int rating) {
+		this.numVoilier = numVoilier;
+		this.nomVoilier = nomVoilier;
+		this.classeVoilier = classeVoilier;
+	}
 }
