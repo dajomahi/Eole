e832ccc52cbf4141abdf5cfba3568dfcae94fbbf
TerminatorXRobocop <kuborosu@gmail.com>
2015-11-09 10:49:44 +0100

    Update du layout

--- Interface Dev/FenGestion.java
+++ Interface Dev/FenGestion.java
@@ -1,4 +1,5 @@
 import java.awt.Dimension;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,13 +10,14 @@ public class FenGestion extends JFrame{
 	
 	private JButton[] buttons = new JButton[20]; 
 	private JPanel p;
-	private Layout l;
+	private GridLayout l = new GridLayout(0,10);
 	
 	public FenGestion(){
 		//preprossesing
 		this.setPreferredSize(new Dimension(700,500));
 		this.setTitle("Eole Gestion");
 		this.getContentPane().add(p);
+		p.setLayout(l);
 		//post processing
 		this.pack();
 		this.setLocationRelativeTo(null);
