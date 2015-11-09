317086993d2bafc8e73477012cb3b22b598170a6
daudecha <damien700.charpentier@laposte.net>
2015-11-09 11:31:14 +0100

    Ajout du dossier application

---
+++ AjoutVoilier/Voilier.java
@@ -0,0 +1,14 @@
+
+public class Voilier {
+	private int numVoilier;
+	private String nomVoilier;
+	private int classeVoilier;
+	private int rating;
+	
+	public Voilier(int numVoilier, String nomVoilier, int classeVoilier, int rating) {
+		this.numVoilier = numVoilier;
+		this.nomVoilier = nomVoilier;
+		this.classeVoilier = classeVoilier;
+		this.rating = rating;
+	}
+}
