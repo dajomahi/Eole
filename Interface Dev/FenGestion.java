fdc41947490870231f9f5287121594f87e16ed2b
TerminatorXRobocop <kuborosu@gmail.com>
2015-11-09 11:29:33 +0100

    T

--- Interface Dev/FenGestion.java
+++ Interface Dev/FenGestion.java
@@ -1,5 +1,7 @@
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,7 +11,7 @@ import javax.swing.JPanel;
 public class FenGestion extends JFrame{
 	
 	private JButton[] buttons = new JButton[20]; 
-	private JPanel p;
+	private JPanel p = new JPanel();
 	private GridLayout l = new GridLayout(0,10);
 	
 	public FenGestion(){
@@ -18,11 +20,27 @@ public class FenGestion extends JFrame{
 		this.setTitle("Eole Gestion");
 		this.getContentPane().add(p);
 		p.setLayout(l);
+		initButtons();
 		//post processing
 		this.pack();
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
 		
+	}
+	public void initButtons(){
+		for(int i = 0; i < buttons.length; i++){
+			buttons[i] = new JButton();
+			buttons[i].setText(i+"");
+			buttons[i].addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					// TODO Auto-generated method stub
+					
+				}
+			});
+			p.add(buttons[i]);
+		}
 	}
 	
 }
