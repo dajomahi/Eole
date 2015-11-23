e9506aa46518add1ab68a4236403f558e494a356
Damien <damien700.charpentier@laposte.net>
2015-11-23 12:05:40 +0100

    Gestion de la fonction tempsReel() dans Voilier.

--- App/app/Conversion.java
+++ App/app/Conversion.java
@@ -1,22 +1,46 @@
 package app;
 
+import java.util.regex.Pattern;
+
 public class Conversion {
 	
-	public static int ConversionSec(String H, String M, String S) {
-		int hours= Integer.parseInt(H.substring(0,1)) * 3600;
-		int minutes= Integer.parseInt(M.substring(0,1)) * 60;
-		int seconds= Integer.parseInt(S.substring(0,1)) + minutes + hours;
-		return seconds;
+	public static int HHMMSSToseconds(String duree) {
+			if(Pattern.matches("\\d\\d:\\d\\d:\\d\\d", duree)){
+				int hours= Integer.parseInt(duree.substring(0,2)) * 3600;
+				int minutes= Integer.parseInt(duree.substring(3,5)) * 60;
+				int seconds= Integer.parseInt(duree.substring(6,8)) + minutes + hours;
+				return seconds;
+			}else{
+				return 0;
+			}
 		}
 	
-	public static int TempsCompense(String H, String M, String S, int Rating, int Distance){
+	public static String secondsToHHMMSS(int secondes) {
+		String duree = "";
+		
+		if(secondes / 3600 <= 0){
+			duree += "00:";
+		} else {
+			duree += secondes / 3600 + ":";
+		}
+		secondes %= 3600;
+		if(secondes / 60 <= 0){
+			duree += "00:";
+		} else {
+			duree += secondes / 60 + ":";
+		}
+		secondes %= 60;
+		duree += secondes + "";
+		
+		return duree;
+	}
+	
+	public static int TempsCompense(String duree, int rating, double distance){
 		int tempsCompense;
-		int distance= Distance;
-		int r= Rating;
-		int hours= Integer.parseInt(H.substring(0,1)) * 3600;
-		int minutes= Integer.parseInt(M.substring(0,1)) * 60;
-		int seconds= Integer.parseInt(S.substring(0,1)) + minutes + hours;
-		int handicap = (int) ((5143 / (Math.sqrt(r) + 3.5)) * distance);
+		int hours= Integer.parseInt(duree.substring(0,2)) * 3600;
+		int minutes= Integer.parseInt(duree.substring(3,5)) * 60;
+		int seconds= Integer.parseInt(duree.substring(6,8)) + minutes + hours;
+		int handicap = (int) ((5143 / (Math.sqrt(rating) + 3.5)) * distance);
 		if (seconds==0){
 			tempsCompense=0;
 		}else{
--- App/app/Voilier.java
+++ App/app/Voilier.java
@@ -4,13 +4,15 @@ import java.util.regex.Pattern;
 
 
 public class Voilier {
-	private static String HEUREDEPART = "00:00:00";
+	private static double distanceEnMiles = 0.0;
+	private static String heureDepart = "00:00:00";
 	private String heureArrivee = "00:00:00";
 	
 	private int numVoilier;
 	private String nomVoilier;
 	private int classeVoilier;
 	private int rating;
+	private boolean handicap = false;
 	
 	public Voilier(int numVoilier, String nomVoilier, int classeVoilier, int rating) {
 		this.numVoilier = numVoilier;
@@ -20,7 +22,7 @@ public class Voilier {
 	}
 
 	public static String getHeureDepart() {
-		return HEUREDEPART;
+		return heureDepart;
 	}
 	
 	/**
@@ -30,7 +32,7 @@ public class Voilier {
 	 */
 	public static boolean setHeureDepart(String heureDepart) {
 		if(Pattern.matches("\\d\\d:\\d\\d:\\d\\d", heureDepart)){
-			Voilier.HEUREDEPART = heureDepart;
+			Voilier.heureDepart = heureDepart;
 			return true;
 		}else{
 			return false;
@@ -71,5 +73,40 @@ public class Voilier {
 		return rating;
 	}
 	
+	public static double getDistanceEnMiles() {
+		return distanceEnMiles;
+	}
+
+	public static void setDistanceEnMiles(double distanceEnMiles) {
+		Voilier.distanceEnMiles = distanceEnMiles;
+	}
+	
+	public String tempsReel(){
+		int hDuree = Integer.parseInt(heureArrivee.substring(0, 2)) - Integer.parseInt(heureDepart.substring(0, 2));
+		int minDuree = Integer.parseInt(heureArrivee.substring(3, 5)) - Integer.parseInt(heureDepart.substring(3, 5));
+		int secDuree = Integer.parseInt(heureArrivee.substring(6, 8)) - Integer.parseInt(heureDepart.substring(6, 8));
+		int dureeFinale = Conversion.HHMMSSToseconds(hDuree + ":" + minDuree + ":" + secDuree);
+		
+		String duree = "";
+		if(dureeFinale / 3600 <= 0){
+			duree += "00:";
+		} else {
+			duree += dureeFinale / 3600 + ":";
+		}
+		dureeFinale %= 3600;
+		if(dureeFinale / 60 <= 0){
+			duree += "00:";
+		} else {
+			duree += dureeFinale / 60 + ":";
+		}
+		dureeFinale %= 60;
+		duree += dureeFinale + "";
+		
+		if(this.handicap){
+			return Conversion.secondsToHHMMSS(Conversion.TempsCompense(duree, rating, distanceEnMiles));
+		}else{
+			return Conversion.secondsToHHMMSS(dureeFinale);
+		}
+	}
 	
 }
